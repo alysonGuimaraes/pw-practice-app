dd7d69c02a5c3076bb5c579994be8fe2a31c1000
Alyson Guimaraes <alyson.guima002@gmail.com>
2025-03-25 21:50:21 -0300

    Novas alterações aula 26

--- app-tests/tests/InteractingWithWebElements.spec.ts
+++ app-tests/tests/InteractingWithWebElements.spec.ts
@@ -34,3 +34,29 @@ test('Locator sintax rules', async({page}) => {
     // by exact text match
     page.locator(':text-is("USing the Grid")')
 })
+
+test('User facing locators', async({page}) => {
+    await page.getByRole('textbox', {name: "Email"}).first().click()
+
+    await page.getByRole('button', {name: "Sign In"}).first().click()
+
+    await page.getByLabel('Email').first().click()
+
+    await page.getByPlaceholder('Jane Doe').click()
+
+    await page.getByText('Using the Grid').click()
+
+    await page.getByTestId('SignIn').click()
+
+    await page.getByTitle('IoT Dashboard').click()
+})
+
+test('locating child elements', async({page}) => {
+    await page.locator('nb-card nb-radio :text-is("Option 1")').click()
+
+    await page.locator('nb-card').locator('nb-radio').locator(':text-is("Option 2")').click()
+
+    await page.locator('nb-card').getByRole('button', {name: 'Sign In'}).first().click()
+
+    await page.locator('nb-card').nth(3).getByRole('button').click()
+})
